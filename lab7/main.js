011b1c1e42953c527682ce78b3a2edd0f6262595
paavill <pavelchist2001@gmail.com>
2022-05-25 03:58:08 +0300

    lab7 finished

--- lab7/main.js
+++ lab7/main.js
@@ -1,3 +1,4 @@
+let functionVariant;
 
 //#region Функции
 function f1(x){
@@ -12,6 +13,17 @@ function f3(x){
     return Math.sqrt(Math.pow(x, 3) + 6*x)/(Math.pow(x, 5) - 4*x)
 }
 
+function setRoot(result, func, optionName){
+    if(func.allOptions === undefined){
+        let allOptions = {}
+        allOptions[optionName] = result
+        result.allOptions = allOptions
+    } else {
+        func.allOptions[optionName] = result
+        result.allOptions = func.allOptions
+    }
+}
+
 function memoization(func){
     let memory = []
     
@@ -21,11 +33,13 @@ function memoization(func){
         return result        
     }
 
+    setRoot(result, func, 'memoization')
+
     result.getValuesNumber = function(){
         return memory.length
     }
 
-    result.getValuesWithIndex = function(index){
+    result.getValueWithIndex = function(index){
         return memory[index]
     }
 
@@ -33,13 +47,17 @@ function memoization(func){
 }
 
 function debug(func){
-    return function(x){
+    let result = function(x){
         console.log('Time: '+ new Date().toTimeString())
         console.log('x: '+ x)
         let result = func(x)
         console.log('y: '+ result)
         return result        
     }
+
+    setRoot(result, func, 'debug')
+
+    return result
 }
 
 function callsCount(func){
@@ -50,6 +68,8 @@ function callsCount(func){
         return func(x)
     }
 
+    setRoot(result, func, 'callsCount')
+
     result.getCallsCount = function(){
         return count
     }
@@ -57,27 +77,61 @@ function callsCount(func){
     result.getSetCallsCount = function(value){
         count = value
     }
-
+    
     return result
 }
 
 //#endregion
 
+function memoizationGetValue(indexStr){
+    alert(functionVariant.allOptions.memoization.getValueWithIndex(parseInt(indexStr)))
+}
+
+function memoizationGetValuesCount(){
+    alert(functionVariant.allOptions.memoization.getValuesNumber())
+}
+
+function callsCountGetCollsNumber(){
+    alert(functionVariant.allOptions.callsCount.getCallsCount())
+}
+
+function setCallsCountZero(){
+    functionVariant.allOptions.callsCount.getSetCallsCount(0)
+}
+
 function inputChanged(prop, value, input){
     input.setAttribute(prop, value)
 }
 
+function showIfChecked(checkBox, col){
+    if(!checkBox.checked){
+        col.setAttribute('class', col.getAttribute('class') + " hidden")
+    } else {
+        col.setAttribute('class', col.getAttribute('class').replace(' hidden', ''))
+    }
+}
+
+function setDisabledPropButtons(value){
+    let buttons = document.getElementsByClassName('btn')
+    buttons = Array.from(buttons)
+    buttons.forEach(e => e.disabled = value)
+}
+
 function calculate(form, functionForm, characForm, optionsForm){
+    setDisabledPropButtons(true)
     let min = form.min.value
     let max = form.max.value
     let h = form.h.value
     let func = getFunction(functionForm)
     let options = getOptions(optionsForm)
-    func = applyOptions(func, options)
+    functionVariant = applyOptions(func, options)
+    showOptionsDialogs(optionsForm)
     let charact = getCharacteristics(characForm)
-    let funcValues = getFunctionValues(func, parseFloat(min), parseFloat(max), parseFloat(h))
+    let funcValues = getFunctionValues(functionVariant, parseFloat(min), parseFloat(max), parseFloat(h))
     let res = checkСharacteristics(funcValues, charact)
     displayCharacteristics('characteristicsResult', res, document.getElementById('mainRow'))
+    setDisabledPropButtons(false)
+
 }
 
 function getFunction(functionForm){
@@ -141,6 +195,11 @@ function getCharacteristics(characForm){
         result.push({key: "Negative: ", func: nerativeValueNumberCharacteristic})
     }
     return result
+}
+
+function showOptionsDialogs(optionsForm){
+    showIfChecked(optionsForm.memoization, document.getElementById('memoization-col'))
+    showIfChecked(optionsForm.callsNumber, document.getElementById('calls-number-col'))
 }
 
 function getOptions(optionsForm){
